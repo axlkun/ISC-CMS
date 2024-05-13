3e1c425c962addf585f6b61205c3bf735e79d061
axlkun <axelcruz.dev@gmail.com>
2024-05-13 10:30:17 -0600

    fix table name

--- database/migrations/2023_10_20_162549_create_project_service_table.php
+++ database/migrations/2023_10_20_162549_create_project_service_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('projects_services', function (Blueprint $table) {
+        Schema::create('project_service', function (Blueprint $table) {
             $table->id();
             $table->foreignId('project_id')->constrained();
             $table->foreignId('service_id')->constrained();
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('projects_services');
+        Schema::dropIfExists('project_service');
     }
 };
